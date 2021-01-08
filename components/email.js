d972750d9ae7574808b7d2f58863f6d96be07d1f
James Wallis <j@wallis.dev>
2021-01-08 22:05:46 +0000

    Change Wallis Consultancy to be Wallis Family Mediation
    Signed-off-by: James Wallis <j@wallis.dev>

--- components/email.js
+++ components/email.js
@@ -2,10 +2,10 @@ import { FaEnvelope } from 'react-icons/fa';
 
 export default function Email() {
   return (
-    <a href="mailto:mw@wallisconsultancy.co.uk" className="flex items-center m-2">
+    <a href="mailto:mw@wallisfamilymediation.co.uk" className="flex items-center m-2">
       <FaEnvelope />
       <p className="ml-2">
-        Email: mw@wallisconsultancy.co.uk
+        Email: mw@wallisfamilymediation.co.uk
       </p>
     </a>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -6,16 +6,16 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <DefaultSeo
-        titleTemplate="%s | Wallis Consultancy"
+        titleTemplate="%s | Wallis Family Mediation"
         description="Mike Wallis is an experienced personable professional who specialises in Mediation and Business Support."
         openGraph={{
           type: 'website',
           locale: 'en_IE',
-          url: 'https://wallisconsultancy.co.uk/',
-          site_name: 'Wallis Consultancy',
+          url: 'https://wallisfamilymediation.co.uk/',
+          site_name: 'Wallis Family Mediation',
           images: [
             {
-              url: 'https://wallisconsultancy.co.uk/images/mike-wallis2.png',
+              url: 'https://wallisfamilymediation.co.uk/images/mike-wallis2.png',
               width: 296,
               height: 330,
               alt: 'Mike Wallis',
